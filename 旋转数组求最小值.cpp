d7e66a99a0c5b2f00b263041993c3a33fba2d8c6
haomeihong <18221050749@139.com>
2016-12-27 09:36:34 +0800

    12-27
    处理特殊情况1101111
    *start=*mid=*end

--- 旋转数组求最小值.cpp
+++ 旋转数组求最小值.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+int find(int * arr,int end);
 int findMin(int *arr,int len){
 	int start=0;
 	int end=len-1;
@@ -8,6 +9,9 @@ int findMin(int *arr,int len){
 	}
 		int index=(start+end)/2;
 		while(start+1<end){
+		if(*(arr+index)==*(arr+start)&&*(arr+index)==*(arr+end)){
+			return find(arr+start,end);
+		}
 		if(*(arr+index)>=*(arr+start) ){
 			start=index;
 		}
@@ -18,7 +22,16 @@ int findMin(int *arr,int len){
 		}
 	return *(arr+end);	
 }
+int find(int * arr,int end){
+	int v=*arr;
+	for(int i=1;i<=end;i++){
+		if(*(arr+i)<v){
+			v=*(arr+i);
+		}
+	}
+	return v;
+}
 int main(int len,char* argue[]){
-int arr[]={3,4,5,6,7,0,1,2};
-cout<<findMin(arr,8)<<endl;	
+int arr[]={1,1,0,1,1,1,1};
+cout<<findMin(arr,7)<<endl;	
 }
